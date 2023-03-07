7e0b44de3185fecf31695e8027ae2b2091e20ce3
bendichsm22pv191 <serg.bendich@gmail.com>
2023-03-07 10:40:08 +0300

    tasks1234 change3

--- src/main/java/BoardSolution.java
+++ src/main/java/BoardSolution.java
@@ -1,51 +1,49 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class BoardSolution {
-    //return true if symbol x in char array a, else false
-    public boolean isSymbolInSymbArr(char symbol, char[] symbolArr, int N) {
-        for (int i = 0; i < N; ++i) {
-            if (symbol == symbolArr[i])
+    public static final int BOARD_SIDE = 9;
+    public static final char[] SET_BOARD_ELEMENTS = new char[]{'1', '2', '3', '4', '5', '6', '7', '8', '9', '.'};
+    public static final int POWER_SET_BOARD_ELEMENTS = 10;
+    protected boolean isSymbolInBoardSet(char symbol) {
+        for (int i = 0; i < POWER_SET_BOARD_ELEMENTS; ++i) {
+            if (symbol == SET_BOARD_ELEMENTS[i])
                 return true;
         }
         return false;
     }
 
-    public char[][] readBoard(int N) {
-        char[][] board = new char[N][N];
-        Scanner in = new Scanner(System.in);
-        for (int i = 0; i < 9; ++i)
-            for (int j = 0; j < 9; ++j) {
-                String c = in.next();
-                if (c.length() == 1) {
-                    board[i][j] = c.charAt(0);
-                }
-            }
-        return board;
+    public char[][] getBoard() {
+        return new char[][]{{'5', '3', '.', '.', '7', '.', '.', '.', '.'}
+                , {'6', '.', '.', '1', '9', '5', '.', '.', '.'}
+                , {'.', '9', '8', '.', '.', '.', '.', '6', '.'}
+                , {'8', '.', '.', '.', '6', '.', '.', '.', '3'}
+                , {'4', '.', '.', '8', '.', '3', '.', '.', '1'}
+                , {'7', '.', '.', '.', '2', '.', '.', '.', '6'}
+                , {'.', '6', '.', '.', '.', '.', '2', '8', '.'}
+                , {'.', '.', '.', '4', '1', '9', '.', '.', '5'}
+                , {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
     }
 
-    //return true if the element 'a' corresponds to the conditions of the matrix 'board', else false
-    public boolean isBoardDigitSymbLocatedInRowOrColumn(char symbol, char[][] board, int N, int columnNum, int rowNum) {
-        for (int k = columnNum + 1; k < N; ++k) {
-            if ((symbol != '.') && (symbol == board[k][rowNum])) {
-                return false;
+    protected boolean isBoardDigitSymbLocatedInRowOrColumn(char symbol, char[][] board, int columnNum, int rowNum) {
+        if (symbol != '.')
+            for (int i = columnNum + 1, j = rowNum + 1; i < BOARD_SIDE || j < BOARD_SIDE; ++i, ++j) {
+                if ((i < BOARD_SIDE) && (symbol == board[i][rowNum])) {
+                    return false;
+                }
+                if ((j < BOARD_SIDE) && (symbol == board[columnNum][j])) {
+                    return false;
+                }
             }
-        }
-        for (int k = rowNum + 1; k < N; ++k) {
-            if ((symbol != '.') && (symbol == board[columnNum][k])) {
-                return false;
-            }
-        }
         return true;
     }
 
-    //return true if the matrix is valid according to the underlying rules, else false
     public String isValidBoard(char[][] board) {
-        int N = 9;
-        char[] conditions = new char[]{'1', '2', '3', '4', '5', '6', '7', '8', '9', '.'};
-        for (int i = 0; i < N; ++i) {
-            for (int j = 0; j < N; ++j) {
-                if (!(isSymbolInSymbArr(board[i][j], conditions, 10) &&
-                        isBoardDigitSymbLocatedInRowOrColumn(board[i][j], board, N, i, j))) {
+        for (int i = 0; i < BOARD_SIDE; ++i) {
+            for (int j = 0; j < BOARD_SIDE; ++j) {
+                if (!(isSymbolInBoardSet(board[i][j]) &&
+                        isBoardDigitSymbLocatedInRowOrColumn(board[i][j], board, i, j))) {
                     return "Matrix is not valid";
                 }
             }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         BoardSolution boardSolution = new BoardSolution();
-        int N = 9;
-        char[][] board = boardSolution.readBoard(N);
+//        char[][] board = boardSolution.readBoard();
+        char[][] board = boardSolution.getBoard();
         System.out.println(boardSolution.isValidBoard(board));
     }
 }
