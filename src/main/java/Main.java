96c224adcb27efc251abfb9865abdd1b8d35a38b
bendichsm22pv191 <serg.bendich@gmail.com>
2023-03-21 11:35:55 +0300

    json task change 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,7 @@
 import json_solution.json_parser.JsonParser;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-//        BoardSolution boardSolution = new BoardSolution();
-//        char[][] board = boardSolution.getBoard();
-//        System.out.println(boardSolution.isValidBoard(board));
+    public static void main(String[] args) {
         String jsonAddress = "https://api.ipify.org/?format=json";
         JsonParser jPars = new JsonParser();
         jPars.findJsonSolution(jsonAddress);
---
+++ src/main/java/other_projects/Main.java
@@ -0,0 +1,14 @@
+//import json_solution.json_parser.JsonParser;
+//
+//import java.io.IOException;
+//
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BoardSolution boardSolution = new BoardSolution();
+//        char[][] board = boardSolution.getBoard();
+//        System.out.println(boardSolution.isValidBoard(board));
+//
+//
+//    }
+//
+//}
