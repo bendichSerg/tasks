cdcb9fb85fdc651319cc1fc2ac2cb41a466a76d2
bendichsm22pv191 <serg.bendich@gmail.com>
2023-03-27 10:19:58 +0300

    replaced the logger with sout

--- src/main/java/jsonsolution/strategy/impl/FileOutputStrategy.java
+++ src/main/java/jsonsolution/strategy/impl/FileOutputStrategy.java
@@ -1,12 +1,10 @@
 package jsonsolution.strategy.impl;
 
 import jsonsolution.strategy.PrintJsonStrategy;
-import lombok.extern.slf4j.Slf4j;
 
 import java.io.FileWriter;
 import java.io.IOException;
 
-@Slf4j
 public class FileOutputStrategy implements PrintJsonStrategy {
     private static final String FILE_NAME = "output.txt";
 
